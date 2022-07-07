7944cfd32916d8157164b11f05c9b029c83bb0e0
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-07-07 22:29:20 +0200

    Update front

--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import { Greeter } from 'react-just-parallax';
+import { Greeter } from 'react-just-parallax';
 
 import { Head } from 'seo/Head/Head';
 
 import * as S from './IndexPage.styles';
 
 export default function IndexPage() {
-  // React.useEffect(() => {
-  //   console.log(Greeter('test'));
-  // }, []);
+  React.useEffect(() => {
+    console.log(Greeter('test4'));
+  }, []);
 
   return (
     <>
