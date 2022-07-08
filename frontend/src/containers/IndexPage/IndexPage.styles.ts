a6e879fdbc54865db1159947de5a12cb796adbf1
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-07-08 09:16:10 +0200

    Update frontend styles

--- frontend/src/containers/IndexPage/IndexPage.styles.ts
+++ frontend/src/containers/IndexPage/IndexPage.styles.ts
@@ -1,14 +1,64 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { media } from 'utils/media';
 
 export const Wrapper = styled.div`
   margin: 0 auto;
   width: 80%;
-  margin-top: 8rem;
+  margin-top: 20rem;
+  margin-bottom: 20rem;
 
   ${media.tablet} {
     width: 100rem;
-    margin-top: 15rem;
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 40rem;
+  margin-top: 15rem;
+  position: relative;
+
+  &:last-child {
+    margin-bottom: 15rem;
+  }
+
+  &:nth-child(1) {
+    background-color: red;
+  }
+  &:nth-child(2) {
+    background-color: purple;
+  }
+  &:nth-child(3) {
+    background-color: green;
+  }
+  &:nth-child(4) {
+    background-color: violet;
+  }
+`;
+
+interface BoxProps {
+  $floating?: boolean;
+}
+
+export const Box = styled.div<BoxProps>`
+  width: 100px;
+  position: relative;
+  background: black;
+
+  ${props =>
+    props.$floating &&
+    css`
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    `}
+
+  &:before {
+    content: '';
+    display: block;
+    width: 100%;
+    padding-bottom: 100%;
   }
 `;
--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -13,7 +13,20 @@ export default function IndexPage() {
   return (
     <>
       <Head />
-      <S.Wrapper style={{ fontSize: 16 }}>test</S.Wrapper>
+      <S.Wrapper style={{ fontSize: 16 }}>
+        <S.Container>
+          <S.Box />
+        </S.Container>
+        <S.Container>
+          <S.Box $floating />
+        </S.Container>
+        <S.Container>
+          <S.Box />
+        </S.Container>
+        <S.Container>
+          <S.Box $floating />
+        </S.Container>
+      </S.Wrapper>
     </>
   );
 }
