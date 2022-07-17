c21d8e654072fde1229ee4d25a9d6eea062a45c1
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-07-18 00:00:25 +0200

    Update styles

--- frontend/src/containers/IndexPage/IndexPage.styles.ts
+++ frontend/src/containers/IndexPage/IndexPage.styles.ts
@@ -2,6 +2,15 @@ import styled, { css } from 'styled-components';
 
 import { media } from 'utils/media';
 
+export const ScrollContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+`;
+
 export const Wrapper = styled.div`
   margin: 0 auto;
   width: 80%;
--- frontend/src/containers/IndexPage/IndexPage.tsx
+++ frontend/src/containers/IndexPage/IndexPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MouseParallax, ScrollParallax } from 'react-just-parallax';
 
 import { Head } from 'seo/Head/Head';
@@ -6,31 +6,35 @@ import { Head } from 'seo/Head/Head';
 import * as S from './IndexPage.styles';
 
 export default function IndexPage() {
+  const scrollContainerRef = useRef<null | HTMLDivElement>(null);
+
   return (
     <>
       <Head />
-      <S.Wrapper style={{ fontSize: 16 }}>
-        <S.Container>
-          <MouseParallax>
-            <S.Box />
-          </MouseParallax>
-        </S.Container>
-        <S.Container>
-          <MouseParallax strength={-0.1}>
-            <S.Box $floating />
-          </MouseParallax>
-        </S.Container>
-        <S.Container>
-          <ScrollParallax>
-            <S.Box />
-          </ScrollParallax>
-        </S.Container>
-        <S.Container>
-          <ScrollParallax>
-            <S.Box $floating />
-          </ScrollParallax>
-        </S.Container>
-      </S.Wrapper>
+      <S.ScrollContainer ref={scrollContainerRef}>
+        <S.Wrapper>
+          <S.Container>
+            <MouseParallax scrollContainerRef={scrollContainerRef}>
+              <S.Box />
+            </MouseParallax>
+          </S.Container>
+          <S.Container>
+            <MouseParallax scrollContainerRef={scrollContainerRef} strength={-0.1}>
+              <S.Box $floating />
+            </MouseParallax>
+          </S.Container>
+          <S.Container>
+            <ScrollParallax scrollContainerRef={scrollContainerRef}>
+              <S.Box />
+            </ScrollParallax>
+          </S.Container>
+          <S.Container>
+            <ScrollParallax scrollContainerRef={scrollContainerRef}>
+              <S.Box $floating />
+            </ScrollParallax>
+          </S.Container>
+        </S.Wrapper>
+      </S.ScrollContainer>
     </>
   );
 }
